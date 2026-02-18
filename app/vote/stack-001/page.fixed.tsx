772797432a442066f7184ef05d6847bd4d07fd21
mjbot68 <mjbot68@gmail.com>
2026-02-17 19:04:25 -0500

    Create page.fixed.tsx

---
+++ app/vote/stack-001/page.fixed.tsx
@@ -0,0 +1,8 @@
+import VoteStack001Client from './VoteStack001Client.jsx';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function Page() {
+  return <VoteStack001Client />;
+}
